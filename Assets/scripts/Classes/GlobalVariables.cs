35e50e9c7e5da0a9d68c3854d9481881b90435a6
vrprojects <vignesh.ravi@mail.utoronto.ca>
2016-09-19 00:50:05 -0400

    Meeting changes - changes to ship stats, kraken stats and line on kraken.

--- Assets/scripts/Classes/GlobalVariables.cs
+++ Assets/scripts/Classes/GlobalVariables.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public static class GlobalVariables {
 
-    public static float gameSpeed = 1.5f;
+    public static float gameSpeed = 1.25f;
 	public static Dictionary<string,string> shipToPrefabLocation = new Dictionary<string, string> () {
 		{"blackbeard","Ship/Blackbeard Ship"},
 		{"atlantean","Ship/Atlantean Ship"},
