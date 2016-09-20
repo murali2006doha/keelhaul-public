560c8106cb79ec9a374791bbe6610d925652706a
abhi <abikrish@gmail.com>
2016-09-19 23:52:54 -0400

    removed unused vars

--- Assets/scripts/spitBallCannon.cs
+++ Assets/scripts/spitBallCannon.cs
@@ -17,15 +17,11 @@ public class spitBallCannon : MonoBehaviour
     public GameObject spit;
     public bool canShoot = true;
     public KrakenInput input;
-    public float angleOfCannonShots = 30;
-    float altTimer;
-    public float speed = 1;
     public int baseSpitDamage = 1;
     public int spitDamage = 1;
     public float maxSpitDamage = 4;
     public float maxSpitDistance = 4;
-    float shootDelay, alternateShootDelay = 0.1f;
-    float forceMultiplier = 20f; //Forward facing force multiplier
+    float shootDelay = 0.1f;
     public float maxSpitScale = 4f;
 
     public void setDelays(float shootDelay, float alternateShootDelay)
@@ -92,26 +88,7 @@ public class spitBallCannon : MonoBehaviour
         cannonBall.GetComponent<CannonBall>().gravity = 0f;
 
     }
-
-    public void handleShoot(Vector3 velocity, float speed)
-    {
-        this.velocity = velocity;
-        this.speed = speed;
-		Vector3 shoot_direction = transform.forward;
-        this.transform.rotation = Quaternion.LookRotation(shoot_direction.normalized);
-		if (canShoot && (input.Actions.Fire.RawValue > .5f))
-        {
-            if (shoot_direction.magnitude > 0)
-            {
-                this.Fire();
-                input.vibrate(.15f, .25f);
-                canShoot = false;
-                Invoke("ResetShotRight", shootDelay);
-            }
-        }
-
-    }
-
+		
 	public void clearSpitball() {
 		spit = null;
 		spitTime = 0;
