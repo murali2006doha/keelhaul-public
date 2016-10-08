dc4c429f61a87679ee7f38a84a239821b2fc78ac
vrprojects <vignesh.ravi@mail.utoronto.ca>
2016-10-07 20:06:04 -0400

    added light pillar on barrel fr respawn

--- Assets/scripts/barrel.cs
+++ Assets/scripts/barrel.cs
@@ -3,9 +3,15 @@ using System.Collections;
 
 public class barrel : MonoBehaviour {
 	public GameObject owner;
+    public LightPillar pillar;
 	// Use this for initialization
 	void Start () {
-	
-	}
+        pillar = GetComponentInChildren<LightPillar>();
+    }
+
+    public void activatePillar()
+    {
+        pillar.activatePillar();
+    }
 
 }
