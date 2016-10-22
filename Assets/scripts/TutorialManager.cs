7c106c40e82bc95d0159d2806710b60348fefbfc
vrprojects <vignesh.ravi@mail.utoronto.ca>
2016-10-22 15:53:48 -0400

    Remove unused script things for tutorial

--- Assets/scripts/TutorialManager.cs
+++ Assets/scripts/TutorialManager.cs
@@ -5,29 +5,7 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class TutorialManager : MonoBehaviour {
-	public RawImage krakenTut;
-	public RawImage shipTut;
-	public RawImage splashTut;
-	MovieTexture krakenMov;
-	MovieTexture shipMov;
-	MovieTexture splashMov;
-	bool splashDone = false;
-	public AudioSource waterSounds;
-
-	// Use this for initialization
-
-	void Start () {
-		krakenMov = (MovieTexture)krakenTut.GetComponent<RawImage> ().mainTexture;
-		shipMov = (MovieTexture)shipTut.GetComponent<RawImage> ().mainTexture;
-		splashMov = (MovieTexture)splashTut.GetComponent<RawImage> ().mainTexture;
-	//	Invoke ("startShip", 32);
-		splashMov.Play ();
-		this.GetComponent<AudioSource> ().clip = splashMov.audioClip;
-		this.GetComponent<AudioSource> ().Play ();
-		waterSounds.loop = true;
-	}
-	
-	// Update is called once per frame
+		// Update is called once per frame
 	void Update () {
 
 		if (InputManager.ActiveDevice.Action1.WasReleased || Input.GetKeyUp(KeyCode.Return) || Input.GetKeyUp(KeyCode.A)) {
@@ -36,12 +14,6 @@ public class TutorialManager : MonoBehaviour {
 
 	}
 
-	void startShip(){
-		shipTut.enabled = true;
-		krakenTut.enabled = false;
-		Invoke ("loadMenu", 50);
-		shipMov.Play ();
-	}
 
 	void loadMenu(){
 		Application.LoadLevel ("start2");
