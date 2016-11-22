01341af48b2d5cd3159956a1c7066b3aa1c5c315
abhi <abikrish@gmail.com>
2016-11-21 23:36:23 -0500

    ship input prelim

---
+++ Assets/scripts/ShipInput.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using InControl;
+
+
+public class ShipInput : MonoBehaviour {
+
+
+    public PlayerActions actions { get; set; }
+    public Action<Vector3> onRotateChanged;
+    // Use this for initialization
+    void Start () {
+	    	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (actions == null) {
+            return;
+        }
+        onRotateChanged(new Vector3(actions.Rotate.X, 0f, actions.Rotate.Y));
+       
+
+    }
+}
