00ceec08005f0cecd3037f7bca441622de6cffc0
abhi <abikrish@gmail.com>
2016-11-26 14:02:43 -0500

    Barrel Renamed

---
+++ Assets/scripts/Barrel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barrel : MonoBehaviour
+{
+    public GameObject owner;
+    public LightPillar pillar;
+    public GameObject explosion;
+    PlayerInput currentPlayer;
+
+    // Use this for initialization
+    void Start()
+    {
+        pillar = GetComponentInChildren<LightPillar>();
+        currentPlayer = FindObjectOfType<PlayerInput>();
+    }
+
+    public void activatePillar()
+    {
+        pillar.activatePillar();
+    }
+
+
+    public void explodeBarrel()
+    {
+        GameObject exp = (GameObject)Instantiate(explosion, this.gameObject.transform.position, Quaternion.identity);
+
+
+    }
+
+}
