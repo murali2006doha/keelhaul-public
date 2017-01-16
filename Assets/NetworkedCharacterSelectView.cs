62b6871e77e203e7ae21be6f63fe03638a045dd6
abhi <abikrish@gmail.com>
2017-01-16 15:36:18 -0500

    Added a a couple view scripts

---
+++ Assets/NetworkedCharacterSelectView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkedCharacterSelectView : MonoBehaviour {
+
+
+    public NetworkedCharacterSelectButton [] buttons;
+    // Use this for initialization
+   public void Initialize(Action<ShipEnum> onClick) {
+        foreach (NetworkedCharacterSelectButton button in buttons) {
+            button.onClickAction = onClick;
+        }
+    }
+}
