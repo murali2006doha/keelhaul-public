78ebf7385cd279dcbc11d0d9742a9e1333a1e7c3
Murali Komaravolu <Murali@Muralis-MacBook-Pro-2.local>
2017-01-23 18:21:11 -0500

    tabs

--- Assets/scripts/Networking/NetworkManager.cs
+++ Assets/scripts/Networking/NetworkManager.cs
@@ -5,82 +5,82 @@ using UnityEngine.UI;
 public class NetworkManager : MonoBehaviour
 {
 
-	[SerializeField]
-	Text connectionText;
-	public GameObject NetworkedCharacterSelect;
-	public GameInitializer initializer;
-	public bool offlineMode = false;
+  [SerializeField]
+  Text connectionText;
+  public GameObject NetworkedCharacterSelect;
+  public GameInitializer initializer;
+  public bool offlineMode = false;
 
-	void Start()
-	{
+  void Start()
+  {
 
-		PhotonNetwork.logLevel = PhotonLogLevel.ErrorsOnly;
-		PhotonNetwork.ConnectUsingSettings("0.2");
-		PhotonNetwork.offlineMode = offlineMode;
+    PhotonNetwork.logLevel = PhotonLogLevel.ErrorsOnly;
+    PhotonNetwork.ConnectUsingSettings("0.2");
+    PhotonNetwork.offlineMode = offlineMode;
 
-		if (offlineMode) {
-			this.OnJoinedLobby();
-		}    		
-	}
+    if (offlineMode) {
+      this.OnJoinedLobby();
+    }
+  }
 
-	void Update()
-	{
-		if (connectionText) {
-			connectionText.text = PhotonNetwork.connectionStateDetailed.ToString();
-		}
+  void Update()
+  {
+    if (connectionText) {
+      connectionText.text = PhotonNetwork.connectionStateDetailed.ToString();
+    }
 
-	}
+  }
 
-	void OnJoinedLobby()
-	{
-		RoomOptions ro = new RoomOptions() { isVisible = true, maxPlayers = 4 };
-		PhotonNetwork.JoinOrCreateRoom("default", ro, TypedLobby.Default);
-	}
+  void OnJoinedLobby()
+  {
+    RoomOptions ro = new RoomOptions() { isVisible = true, maxPlayers = 4 };
+    PhotonNetwork.JoinOrCreateRoom("default", ro, TypedLobby.Default);
+  }
 
-	void OnJoinedRoom()
-	{
-		if (PhotonNetwork.offlineMode)
-		{
-			initializer.isMaster = true;
-			initializer.playerId = 1;
-			initializer.enabled = true;
-			initializer.gameObject.SetActive(true);
-			initializer.onGameManagerCreated = this.onGameManagerCreated;
-		}
-		else {
+  void OnJoinedRoom()
+  {
+    if (PhotonNetwork.offlineMode)
+    {
+      initializer.isMaster = true;
+      initializer.playerId = 1;
+      initializer.enabled = true;
+      initializer.gameObject.SetActive(true);
+      initializer.onGameManagerCreated = this.onGameManagerCreated;
+    }
+    else {
 
-			GameObject instantiated = Instantiate(NetworkedCharacterSelect);
-			AbstractCharacterSelectController csc = instantiated.GetComponent<AbstractCharacterSelectController> ();
-			csc.OnSelectCharacterAction(
-				() => {
-					csc.setPlayerSelectSettings ();					
-						
-					foreach(CharacterSelection cs in csc.getPlayerSelectSettings().players) {
-						print(cs.selectedCharacter);
-					}
+      GameObject instantiated = Instantiate(NetworkedCharacterSelect);
+      AbstractCharacterSelectController csc = instantiated.GetComponent<AbstractCharacterSelectController> ();
+      csc.OnSelectCharacterAction(
+        () => {
+          csc.setPlayerSelectSettings ();         
+            
+          foreach(CharacterSelection cs in csc.getPlayerSelectSettings().players) {
+            print(cs.selectedCharacter);
+          }
 
-					StartSpawnProcess(csc.getPlayerSelectSettings().players[0].selectedCharacter);
-					print(csc.getPlayerSelectSettings().players[0].selectedCharacter);
-					Destroy(instantiated);
+          StartSpawnProcess(csc.getPlayerSelectSettings().players[0].selectedCharacter);
+          print(csc.getPlayerSelectSettings().players[0].selectedCharacter);
+          Destroy(instantiated);
 
-			});
-		}
-	}
+      });
+    }
+  }
 
-	void StartSpawnProcess(ShipEnum type)
-	{
-		Debug.Log(type);
-		initializer.shipSelections[0].selectedCharacter = type;
-		initializer.isMaster = true;
-		initializer.playerId = PhotonNetwork.player.ID;
-		initializer.onGameManagerCreated = onGameManagerCreated;
-		initializer.enabled = true;
-	}
+  void StartSpawnProcess(ShipEnum type)
+  {
+    Debug.Log(type);
+    initializer.shipSelections[0].selectedCharacter = type;
+    initializer.isMaster = true;
+    initializer.playerId = PhotonNetwork.player.ID;
+    initializer.onGameManagerCreated = onGameManagerCreated;
+    initializer.enabled = true;
+  }
 
-	void onGameManagerCreated() {
-		AbstractGameManager manager = FindObjectOfType<AbstractGameManager>();
-		manager.enabled = true;
-		//   manager.GetComponent<PhotonView>().RPC("AddPlayer", PhotonTargets.All, PhotonNetwork.player.ID);
+  void onGameManagerCreated() {
+    AbstractGameManager manager = FindObjectOfType<AbstractGameManager>();
+    manager.enabled = true;
+    //   manager.GetComponent<PhotonView>().RPC("AddPlayer", PhotonTargets.All, PhotonNetwork.player.ID);
 
-	}
+  }
 }
