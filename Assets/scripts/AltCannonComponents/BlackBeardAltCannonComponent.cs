8daac9c0e37898828a0b33cec91d24a3c8f9e8f0
murali2006doha <Murali@Muralis-MacBook-Pro-2.local>
2017-02-28 21:25:26 -0500

    screen shake for blackbeard alt

--- Assets/scripts/AltCannonComponents/BlackBeardAltCannonComponent.cs
+++ Assets/scripts/AltCannonComponents/BlackBeardAltCannonComponent.cs
@@ -4,95 +4,95 @@ using UnityEngine;
 
 public class BlackBeardAltCannonComponent : AbstractAltCannonComponent {
 
-	public List<Transform> rightCannons = new List<Transform>();
-	public List<Transform> leftCannons = new List<Transform>();
+    public List<Transform> rightCannons = new List<Transform>();
+    public List<Transform> leftCannons = new List<Transform>();
 
-	public int arcCannonForce = 10;
-	public Vector3 velocity;
-	public float dampening = 1f;
-	public float speed = 1;
-	public float minDelay; 
-	public float maxDelay; //will find random time between the two
+    public int arcCannonForce = 10;
+    public Vector3 velocity;
+    public float dampening = 1f;
+    public float speed = 1;
+    public float minDelay; 
+    public float maxDelay; //will find random time between the two
 
-	List<Transform> tempCannonListL;
-	List<Transform> tempCannonListR;
-	Quaternion originalRotation;
+    List<Transform> tempCannonListL;
+    List<Transform> tempCannonListR;
+    Quaternion originalRotation;
 
-	void Start() {
-		originalRotation = this.transform.rotation;
-	}
-
-
-	public override void setupRotation() {
-		shoot_direction = aim.transform.position - shipTransform.position;
-	}
+    void Start() {
+        originalRotation = this.transform.rotation;
+    }
 
 
-	public override void alternateFire() {
-		this.input.followCamera.startShake (Random.Range (0.1f, 0.3f), Random.Range (1f, 3f));
-
-		initializeCannonBalls ();
-
-		shuffle (rightCannons);
-		shuffle (leftCannons);
-
-		StartCoroutine (fireAll(rightCannons));
-		StartCoroutine (fireAll(leftCannons));
-
-		input.gameStats.numOfAlternateShots++;
-
-		resetAllCannons ();
-	}
+    public override void setupRotation() {
+        shoot_direction = aim.transform.position - shipTransform.position;
+    }
 
 
-	IEnumerator fireAll(List<Transform> cannonList) {
+    public override void alternateFire() {
+        this.input.followCamera.startShake (Random.Range (0.1f, 0.3f), Random.Range (1f, 3f));
 
-		int count = cannonList.Count;
-		for (int x = 0; x < count; x++) {
-			float random_delay = Random.Range (minDelay, maxDelay);
-			Transform cannonShot = cannonList [0];			
-			fireShot (cannonShot);
-			cannonList.RemoveAt (0);
+        initializeCannonBalls ();
 
-			yield return new WaitForSeconds (random_delay);
-		}
-	}
+        shuffle (rightCannons);
+        shuffle (leftCannons);
 
+        StartCoroutine (fireAll(rightCannons));
+        StartCoroutine (fireAll(leftCannons));
 
-	void shuffle(List<Transform> cannonList) {
+        input.gameStats.numOfAlternateShots++;
 
-		for (int i = 0; i < cannonList.Count; i++) {
-
-			Transform temp = cannonList [i];
-			int randomCannon = Random.Range (0, cannonList.Count);
-			cannonList [i] = cannonList [randomCannon];
-			cannonList [randomCannon] = temp;
-
-		}
-	}
+        resetAllCannons ();
+    }
 
 
-	void fireShot (Transform cannonPos) {
-		GameObject cannonBall = PhotonNetwork.Instantiate(PathVariables.alternateBlackbeardShot, cannonPos.position + (velocity * dampening), cannonPos.rotation, 0);
+    IEnumerator fireAll(List<Transform> cannonList) {
+
+        int count = cannonList.Count;
+        for (int x = 0; x < count; x++) {
+            float random_delay = Random.Range (minDelay, maxDelay);
+            Transform cannonShot = cannonList [0];          
+            fireShot (cannonShot);
+            cannonList.RemoveAt (0);
+
+            yield return new WaitForSeconds (random_delay);
+        }
+    }
+
+
+    void shuffle(List<Transform> cannonList) {
+
+        for (int i = 0; i < cannonList.Count; i++) {
+
+            Transform temp = cannonList [i];
+            int randomCannon = Random.Range (0, cannonList.Count);
+            cannonList [i] = cannonList [randomCannon];
+            cannonList [randomCannon] = temp;
+
+        }
+    }
+
+
+    void fireShot (Transform cannonPos) {
+        GameObject cannonBall = PhotonNetwork.Instantiate(PathVariables.alternateBlackbeardShot, cannonPos.position + (velocity * dampening), cannonPos.rotation, 0);
         cannonBall.GetComponent<CannonBall>().setOwner(shipTransform);
         cannonBall.GetComponent<PhotonView>().RPC("AddForce", PhotonTargets.All, cannonBall.transform.forward * altCannonForce + cannonBall.transform.up * arcCannonForce);
-	}
+    }
 
 
-	void initializeCannonBalls() {
+    void initializeCannonBalls() {
 
-		tempCannonListL = new List<Transform>(leftCannons);
-		tempCannonListR = new List<Transform>(rightCannons);
+        tempCannonListL = new List<Transform>(leftCannons);
+        tempCannonListR = new List<Transform>(rightCannons);
 
-	}
+    }
 
 
-	void resetAllCannons() {
+    void resetAllCannons() {
 
-		leftCannons = new List<Transform>(tempCannonListL);
-		rightCannons = new List<Transform>(tempCannonListR);
+        leftCannons = new List<Transform>(tempCannonListL);
+        rightCannons = new List<Transform>(tempCannonListR);
 
-	}
-		
+    }
+        
 }
 
--- Assets/scripts/cameraFollow.cs
+++ Assets/scripts/cameraFollow.cs
@@ -3,38 +3,38 @@ using System.Collections;
 
 public class cameraFollow : MonoBehaviour {
 
-	public GameObject target;
-	public Camera camera;
+    public GameObject target;
+    public Camera camera;
     public string cullingMask;
-	public float x_diff, y_diff, z_diff = 0;
-	public bool respawning = false;
-	Vector3 offset;
-	public float respawnFollowSpeed = 20f;
+    public float x_diff, y_diff, z_diff = 0;
+    public bool respawning = false;
+    Vector3 offset;
+    public float respawnFollowSpeed = 20f;
     public float followSpeed = 1f;
-	public string [] cullingMasks;
-	public bool ready;
+    public string [] cullingMasks;
+    public bool ready;
     public float maxFollowSpeed = 3f;
     public float minFollowSpeed = .4f;
     bool targetSet = false;
     public bool zoomIn = false;
     public float zoomInValue, zoomInSpeed = 1f;
     public float normalZoomValue = 3.25f;
-	public float duration = 2f;
-	public float magnitude = .5f;
+    public float duration = 2f;
+    public float magnitude = .5f;
 
-	float origDuration;
-	float origMagnitude;
-	float shakeTime;
-	bool shaking;
+    float origDuration;
+    float origMagnitude;
+    float shakeTime;
+    bool shaking;
     [SerializeField] UnityStandardAssets.ImageEffects.MotionBlur blurEffect;
     CharacterController character;
-	// Use this for initialization
-	void Start () {
-		offset = new Vector3 (x_diff, y_diff, z_diff);
+    // Use this for initialization
+    void Start () {
+        offset = new Vector3 (x_diff, y_diff, z_diff);
         getCharacterController();
-		origDuration = duration;
-		origMagnitude = magnitude;
-	}
+        origDuration = duration;
+        origMagnitude = magnitude;
+    }
 
     void getCharacterController() {
         if (target)
@@ -45,28 +45,28 @@ public class cameraFollow : MonoBehaviour {
         camera.rect = rect;
     }
 
-	public void setCullingMasks()
-	{
-		foreach (string k in cullingMasks) {
-			if (k.Equals (cullingMask) == false) {
-				camera.cullingMask &=  ~(1 << LayerMask.NameToLayer(k));	
+    public void setCullingMasks()
+    {
+        foreach (string k in cullingMasks) {
+            if (k.Equals (cullingMask) == false) {
+                camera.cullingMask &=  ~(1 << LayerMask.NameToLayer(k));    
 
-			}
+            }
 
-		}
+        }
 
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+    
+    // Update is called once per frame
+    void Update () {
         if (!targetSet) {
             if (target) {
                 targetSet = true;
                 transform.position = target.transform.position + offset;
             }
         }
-		if (ready) {
+        if (ready) {
             if (!character)
             {
                 getCharacterController();
@@ -77,9 +77,9 @@ public class cameraFollow : MonoBehaviour {
                 if (!respawning)
                 {
                     transform.position = Vector3.MoveTowards(transform.position, target.transform.position + offset, followSpeed * (Time.deltaTime * GlobalVariables.gameSpeed * velocity));
-					if (shaking) {
-						shake ();
-					}
+                    if (shaking) {
+                        shake ();
+                    }
                 }
 
                 if (zoomIn && camera.orthographicSize > zoomInValue)
@@ -109,43 +109,43 @@ public class cameraFollow : MonoBehaviour {
 
                 }
             }
-			
-		}
+            
+        }
 
-	}
+    }
 
-	public void startShake(){
-		if (!shaking) {
-			shaking = true;
-			shakeTime = Time.realtimeSinceStartup;
-		}
+    public void startShake(){
+        if (!shaking) {
+            shaking = true;
+            shakeTime = Time.realtimeSinceStartup;
+        }
         else
         {
             shakeTime = Time.realtimeSinceStartup;
         }
-	}
+    }
 
-	public void startShake(float randMag, float randDur){
-		duration = randDur;
-		magnitude = randMag;
+    public void startShake(float randMag, float randDur){
+        duration = randDur;
+        magnitude = randMag;
 
-		if (!shaking) {
-			shaking = true;
-			shakeTime = Time.realtimeSinceStartup;
-		}
-		else
-		{
-			shakeTime = Time.realtimeSinceStartup;
-		}
-
-
-	}
+        if (!shaking) {
+            shaking = true;
+            shakeTime = Time.realtimeSinceStartup;
+        }
+        else
+        {
+            shakeTime = Time.realtimeSinceStartup;
+        }
 
 
-	void resetDurationMagnitude() {
-		duration = origDuration;
-		magnitude = origMagnitude;
-	}
+    }
+
+
+    void resetDurationMagnitude() {
+        duration = origDuration;
+        magnitude = origMagnitude;
+    }
 
 
 
@@ -157,27 +157,27 @@ public class cameraFollow : MonoBehaviour {
         blurEffect.enabled = false;
     }
 
-	public void shake(){
+    public void shake(){
 
-		float percentComplete = (Time.realtimeSinceStartup - shakeTime) / duration;         
-	    float damper = 1.0f - Mathf.Clamp(4.0f * percentComplete - 3.0f, 0.0f, 1.0f);    
+        float percentComplete = (Time.realtimeSinceStartup - shakeTime) / duration;         
+        float damper = 1.0f - Mathf.Clamp(4.0f * percentComplete - 3.0f, 0.0f, 1.0f);    
 
-	    float x = Random.value * 2.0f - 1.0f;
-	    float y = Random.value * 2.0f - 1.0f;
-	    x *= magnitude * damper;
-	    y *= magnitude * damper;
-	        
-		transform.position = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + y);
-		if (percentComplete >= 1) {
-			shaking = false;
-			shakeTime = 0;
-		}
-		resetDurationMagnitude ();
-	}
+        float x = Random.value * 2.0f - 1.0f;
+        float y = Random.value * 2.0f - 1.0f;
+        x *= magnitude * damper;
+        y *= magnitude * damper;
+            
+        transform.position = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + y);
+        if (percentComplete >= 1) {
+            shaking = false;
+            shakeTime = 0;
+        }
+        resetDurationMagnitude ();
+    }
 
 
 
-	public void setRespawn(){
-		respawning = true;
-	}
+    public void setRespawn(){
+        respawning = true;
+    }
 }
