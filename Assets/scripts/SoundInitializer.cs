c0ea7b1ef00bd2832174f07dadae22cb7100a228
murali2006doha <Murali@Muralis-MacBook-Pro-2.local>
2017-03-04 15:12:05 -0500

    created another script just for the sound prefab

--- Assets/scripts/SoundInitializer.cs
+++ Assets/scripts/SoundInitializer.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class SoundInitializer : MonoBehaviour {
 
     public AudioClip soundClip;
-    AudioSource audioSource;
     GameObject soundObject;
 
 
@@ -13,22 +12,9 @@ public class SoundInitializer : MonoBehaviour {
     void Start () {
         soundObject = new GameObject();
         soundObject.AddComponent<AudioSource> ();
-        audioSource = soundObject.GetComponent<AudioSource> ();
-
-        //sets the cannonball the child of the empty game object
-        this.transform.SetParent (soundObject.gameObject.transform);
-
-        audioSource.clip = soundClip;
-        StartCoroutine (PlaySound ());
-
+		soundObject.AddComponent<PrefabSound> ();
+		soundObject.GetComponent<AudioSource> ().clip = soundClip;
+		soundObject.GetComponent<PrefabSound> ().StartSound();
     }
-
-
-    IEnumerator PlaySound() {
-        audioSource.Play ();
-        yield return new WaitForSeconds(audioSource.clip.length);
-
-        Destroy (soundObject);
-    }
-
+		
 }
