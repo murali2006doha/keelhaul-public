c9a15c45f2389399fd60a212cc5e1ee7bf03c13c
murali2006doha <murali2006.doha@gmail.com>
2017-03-09 18:31:58 -0500

    effing indents

--- Assets/scripts/Components/BombComponent.cs
+++ Assets/scripts/Components/BombComponent.cs
@@ -5,41 +5,41 @@ using System.Collections.Generic;
 
 public class BombComponent : MonoBehaviour {
 
-	BombControllerComponent parentCannon;
-	PlayerInput player;
+    BombControllerComponent parentCannon;
+    PlayerInput player;
 
-	int blinks = 5;
-	float blinkTime;
+    int blinks = 5;
+    float blinkTime;
 
-	public GameObject largeBombZone;
-	public GameObject smallBombZone;
-	public GameObject bombModel;
+    public GameObject largeBombZone;
+    public GameObject smallBombZone;
+    public GameObject bombModel;
 
-	//these are all the animations involved
-	public float explosion_duration = 1.5f;
-	public float damage;
-	public float waitTimeToExplode;
-	public AudioClip fuseSound;
-	public AudioClip explosionSound;
+    //these are all the animations involved
+    public float explosion_duration = 1.5f;
+    public float damage;
+    public float waitTimeToExplode;
+    public AudioClip fuseSound;
+    public AudioClip explosionSound;
 
 
-	void Start() {
+    void Start() {
 
-		blinkTime = (waitTimeToExplode / (float) blinks);
+        blinkTime = (waitTimeToExplode / (float) blinks);
         transform.rotation = Quaternion.Euler(-90, 0, 0); //prefabs should be adjusted so this is no longer necc
 
     }
 
 
-	internal void Initialize(BombControllerComponent parent, PlayerInput input) {
-		this.parentCannon = parent;
-		this.player = input;
-	}
+    internal void Initialize(BombControllerComponent parent, PlayerInput input) {
+        this.parentCannon = parent;
+        this.player = input;
+    }
 
 
-	public PlayerInput getPlayer() {
-		return player;
-	}
+    public PlayerInput getPlayer() {
+        return player;
+    }
 
     [PunRPC]
     public void DestroySelf() {
@@ -47,38 +47,38 @@ public class BombComponent : MonoBehaviour {
     }
 
 
-	void startSound(AudioClip sound) {
-		this.gameObject.AddComponent<SoundInitializer> ().soundClip = sound;
-	}
+    void startSound(AudioClip sound) {
+        this.gameObject.AddComponent<SoundInitializer> ().soundClip = sound;
+    }
 
-	public IEnumerator ActivateBomb() {
+    public IEnumerator ActivateBomb() {
 
-		startSound (fuseSound);
-		smallBombZone.SetActive (false);
-		largeBombZone.SetActive (true);
+        startSound (fuseSound);
+        smallBombZone.SetActive (false);
+        largeBombZone.SetActive (true);
 
-		for(int i = 0; i < blinks; i++){ //blinks when activated
-			largeBombZone.GetComponent<Renderer> ().material.color = Color.white;
-			bombModel.GetComponent<Renderer> ().material.color = Color.white;
-			yield return new WaitForSeconds(blinkTime);
-			largeBombZone.GetComponent<Renderer> ().material.color = Color.yellow;
-			bombModel.GetComponent<Renderer> ().material.color = Color.red;
-			yield return new WaitForSeconds(blinkTime);
-		}
+        for(int i = 0; i < blinks; i++){ //blinks when activated
+            largeBombZone.GetComponent<Renderer> ().material.color = Color.white;
+            bombModel.GetComponent<Renderer> ().material.color = Color.white;
+            yield return new WaitForSeconds(blinkTime);
+            largeBombZone.GetComponent<Renderer> ().material.color = Color.yellow;
+            bombModel.GetComponent<Renderer> ().material.color = Color.red;
+            yield return new WaitForSeconds(blinkTime);
+        }
 
-		//explode!
-		Destroy (largeBombZone);            //destroy the parameter zone
+        //explode!
+        Destroy (largeBombZone);            //destroy the parameter zone
 
         if (GetComponent<PhotonView>().isMine) {
-			startSound (explosionSound);
+            startSound (explosionSound);
             GameObject exp = explode(); //produces an explosion
                                         //Invoke ("fadeHalo", .5f);
             yield return new WaitForSeconds(explosion_duration);
 
-            Destroy(exp);					//destroy the explosion
+            Destroy(exp);                   //destroy the explosion
         }
         
-	}
+    }
 
     [PunRPC]
     public void RPCActivateBomb()
@@ -88,34 +88,34 @@ public class BombComponent : MonoBehaviour {
 
 
 
-	void OnTriggerEnter(Collider other) {
+    void OnTriggerEnter(Collider other) {
         if (!GetComponent<PhotonView>().isMine)
         {
             return;
         }
 
         if ((other.gameObject.name).Equals ("playerMesh") && 
-			player.gameObject != other.GetComponentInParent<PlayerInput>().gameObject) {//to activate a bomb
+            player.gameObject != other.GetComponentInParent<PlayerInput>().gameObject) {//to activate a bomb
 
  
-			if (parentCannon.getBombList().Contains (other.gameObject) == false) {
+            if (parentCannon.getBombList().Contains (other.gameObject) == false) {
                 player.gameStats.numOfBombsDetonated += 0.5f;
                 GetComponent<PhotonView>().RPC("RPCActivateBomb", PhotonTargets.All);
             }
-		}
+        }
 
-		if (other.gameObject.name.Contains("Kraken")) {
-			KrakenInput kraken = other.gameObject.GetComponent<KrakenInput> ();
-			if (kraken.submerged == false) { //only if not submerged
-				kraken.gameStats.numOfBombsDetonated++;
-				StartCoroutine(ActivateBomb());
-			}
-		}
+        if (other.gameObject.name.Contains("Kraken")) {
+            KrakenInput kraken = other.gameObject.GetComponent<KrakenInput> ();
+            if (kraken.submerged == false) { //only if not submerged
+                kraken.gameStats.numOfBombsDetonated++;
+                StartCoroutine(ActivateBomb());
+            }
+        }
 
-	}
+    }
 
  
-	private GameObject explode(){
+    private GameObject explode(){
         if (gameObject != null)
         {
             Destroy(gameObject);
@@ -125,11 +125,11 @@ public class BombComponent : MonoBehaviour {
             return null;
         }
 
-		GameObject exp = PhotonNetwork.Instantiate(PathVariables.bombExplosionPath, gameObject.transform.position, Quaternion.identity, 0);
+        GameObject exp = PhotonNetwork.Instantiate(PathVariables.bombExplosionPath, gameObject.transform.position, Quaternion.identity, 0);
         exp.GetComponent<PhotonView>().RPC("Initialize", PhotonTargets.All, this.damage, this.player.GetId());
         GetComponent<PhotonView>().RPC("DestroySelf", PhotonTargets.All);
         return exp;
-	}
+    }
 
 
 }
