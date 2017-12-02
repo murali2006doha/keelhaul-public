5649d08954068cd5a2999eb50d8037b6b4b5a12e
vrprojects <vignesh.ravi@mail.utoronto.ca>
2017-12-02 08:20:09 -0800

    stuff maybe needed

---
+++ Assets/scripts/MapSelect/MapSelectController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using InControl;
+using System;
+
+public class MapSelectController : MonoBehaviour {
+
+	[SerializeField]
+    public MapSelectView view;
+
+
+    public void Initialize(PlayerSelectSettings settings, GameTypeEnum gameMode)
+	{
+
+        view.Initialize(gameMode);
+
+	}
+
+    public void OnSelectMap(Action action)
+	{
+        view.OnSelectMap(action);
+	}
+}
